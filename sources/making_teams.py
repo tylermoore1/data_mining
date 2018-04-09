5458340faba4a3768cec2eab8699f10f66789d65
Tyler Moore <tymo1918@colorado.edu>
2018-04-09 16:56:07 -0600

    Added code to create new teams dataset

---
+++ sources/making_teams.py
@@ -0,0 +1,29 @@
+import sys
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+
+data = pd.read_csv('sample.csv')
+
+# new_data = data.groupby(['match_id','team_id']).agg({'player_kills' : 'sum',
+#                                                       'player_dist_walk' : 'mean'})
+
+#all of these uses the groupby function so that we can identify all the unique team id's so that we
+#can find all the team kills and distance traveled and things like that
+game_size = data.groupby(['match_id','team_id'])[['game_size']].mean()
+party_size = data.groupby(['match_id','team_id'])[['party_size']].mean()
+player_dist_ride = data.groupby(['match_id','team_id'])[['player_dist_ride']].mean()
+player_dist_walk = data.groupby(['match_id','team_id'])[['player_dist_walk']].mean()
+player_kills = data.groupby(['match_id','team_id'])[['player_kills']].sum()
+team_placement = data.groupby(['match_id','team_id'])[['team_placement']].mean()
+
+#concat all of the dataframes together so that it is in one table
+result = pd.concat([game_size, party_size, player_dist_ride, player_dist_walk, player_kills, team_placement], axis=1)
+
+#reset the index so that it looks nicely
+new_data = (result.reset_index()[['match_id', 'team_id', 'game_size', 'party_size', 'player_dist_ride', 'player_dist_walk', 'player_kills', 'team_placement']])
+
+
+#read the new team table into the csv file
+new_data.to_csv('team.csv', sep=',')

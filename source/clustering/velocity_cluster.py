3899271d9b04e0545e39d3394ee8c64f85f1cf86
Tyler Moore <tymo1918@colorado.edu>
2018-04-19 21:51:52 -0600

    Updating

---
+++ source/clustering/velocity_cluster.py
@@ -0,0 +1,21 @@
+import sys
+import pandas as pd
+import numpy as np
+import math
+
+cluster0 = 0
+cluster1 = 0.5
+cluster2 = 0.75
+cluster3 = 1
+cluster4 = 1.25
+cluster5 = 1.5
+cluster6 = 1.75
+cluster7 = 2
+cluster8 = 2.25
+cluster9 = 2.5
+cluster10 = 2.75
+cluster11 = 3
+
+data = pd.read_csv('../team_data/team_data.csv')
+
+print (data.head())

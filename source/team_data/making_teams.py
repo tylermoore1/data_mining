3e8446dacca0aa42ed8a049de2296d1214c6ac7a
Joshua-Ramos <ramos14722@gmail.com>
2018-04-19 21:44:11 -0600

    Update making_teams.py
    added speed

--- source/team_data/making_teams.py
+++ source/team_data/making_teams.py
@@ -4,26 +4,25 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
-data = pd.read_csv('sample.csv')
+data = pd.read_csv('aggregate_data.csv')
 
-# new_data = data.groupby(['match_id','team_id']).agg({'player_kills' : 'sum',
-#                                                       'player_dist_walk' : 'mean'})
 
 #all of these uses the groupby function so that we can identify all the unique team id's so that we
 #can find all the team kills and distance traveled and things like that
 game_size = data.groupby(['match_id','team_id'])[['game_size']].mean()
-party_size = data.groupby(['match_id','team_id'])[['party_size']].mean()
+party_size = data.groupby(['match_id','team_id']).size()
 player_dist_ride = data.groupby(['match_id','team_id'])[['player_dist_ride']].mean()
 player_dist_walk = data.groupby(['match_id','team_id'])[['player_dist_walk']].mean()
+player_survive_time = data.groupby(['match_id', 'team_id'])[['player_survive_time']].mean()
 player_kills = data.groupby(['match_id','team_id'])[['player_kills']].sum()
 team_placement = data.groupby(['match_id','team_id'])[['team_placement']].mean()
 
 #concat all of the dataframes together so that it is in one table
-result = pd.concat([game_size, party_size, player_dist_ride, player_dist_walk, player_kills, team_placement], axis=1)
+result = pd.concat([game_size, party_size, player_dist_ride, player_dist_walk, player_survive_time, player_kills, team_placement], axis=1)
 
 #reset the index so that it looks nicely
-new_data = (result.reset_index()[['match_id', 'team_id', 'game_size', 'party_size', 'player_dist_ride', 'player_dist_walk', 'player_kills', 'team_placement']])
+#new_data = (result.reset_index()[['match_id', 'team_id', 'game_size', 'party_size', 'player_dist_ride', 'player_dist_walk', 'player_kills', 'team_placement']])
 
 
 #read the new team table into the csv file
-new_data.to_csv('team.csv', sep=',')
+result.to_csv('team_data.csv', sep=',')
